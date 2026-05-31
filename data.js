c41ea47e2d461e236bdbb232f6f67c838a2e1af3
leefstraatmaurice-lab <leefstraatmaurice@gmail.com>
2026-05-31 17:51:28 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,45 @@
+// ── LEEFSTRAAT MAURICE VERDONCKLAAN ──
+// Pas hier de activiteiten aan en upload naar GitHub
+// fb: voeg een Facebook albumlink toe als die beschikbaar is
+
+const ACTIVITEITEN = [
+  {
+    date: '2026-06-05',
+    name: 'Hot Dogs Party',
+    note: '17:30',
+    fb: ''
+  },
+  {
+    date: '2026-07-11',
+    name: 'Spelletjesavond',
+    note: '',
+    fb: ''
+  },
+  {
+    date: '2026-08-16',
+    name: 'Koffie & taart',
+    note: '',
+    fb: ''
+  },
+  {
+    date: '2026-08-29',
+    name: 'BBQ',
+    note: '',
+    fb: ''
+  },
+  {
+    date: '2026-08-30',
+    name: 'Afbraak Leefstraat',
+    note: '',
+    fb: ''
+  }
+];
+
+// Vaste Facebook albums (altijd zichtbaar op fotopagina)
+const VASTE_ALBUMS = [
+  {
+    name: 'Afscheidsdrink',
+    date: '13 jul 2022',
+    fb: 'https://www.facebook.com/media/set/?set=oa.1089925041954156&type=3'
+  }
+];
